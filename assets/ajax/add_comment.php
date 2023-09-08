e5977bf282b8ceae2adc7437c9360cf89b10d4d3
Kocsis Kriszta <101735682+tincsy01@users.noreply.github.com>
2023-09-08 14:22:39 +0200

    popularity update

--- assets/ajax/add_comment.php
+++ assets/ajax/add_comment.php
@@ -3,30 +3,21 @@ require_once '../../config/config.php';
 require_once '../../config/db_config.php';
 $pdo = connectDatabase($dsn, $pdoOptions);
 
-// Ellenőrizzük, hogy az AJAX kérés POST metódussal érkezett-e
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ellenőrizzük, hogy a szükséges adatokat elküldték-e
     if (isset($_POST['comment']) && isset($_POST['attraction_id']) && isset($_POST['rate'])) {
-        // A POST adatokat változókba mentjük
         $comment = $_POST['comment'];
         $attraction_id = $_POST['attraction_id'];
         $rate = $_POST['rate'];
 
-        // Ellenőrizzük, hogy a felhasználó be van-e jelentkezve
         session_start();
         if (!isset($_SESSION['user_id'])) {
             echo json_encode(['success' => false, 'message' => 'User is not logged in']);
             exit;
         }
-
         $user_id = $_SESSION['user_id'];
-
-        // Az aktuális dátum és idő lekérdezése
         $datetime = date('Y-m-d H:i:s');
 
-        // Írd meg a SQL lekérdezést a komment hozzáadásához és az értékelés mentéséhez az adatbázisban
 
-        // Példa SQL lekérdezés az INSERT utasítás használatával
         $sql = "INSERT INTO comments (user_id, attraction_id, comment, date, evaluation) VALUES (:user_id, :attraction_id, :comment, :date, :rating)";
         $query = $pdo->prepare($sql);
         $query->bindParam(':user_id', $user_id, PDO::PARAM_INT);
@@ -36,11 +27,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $query->bindParam(':rating', $rate, PDO::PARAM_INT);
 
         try {
-            // Kísérlet a komment beszúrására az adatbázisba
             $query->execute();
 
-            // Ellenőrizzük, hogy a beszúrás sikeres volt-e, és válasz üzenetet küldünk vissza
             if ($query->rowCount() > 0) {
+                $average = calculateAttractionAverageRating($attraction_id, $pdo);
+
+                $updateSql = "UPDATE attractions SET popular = :average WHERE attraction_id = :attraction_id";
+                $updateQuery = $pdo->prepare($updateSql);
+                $updateQuery->bindParam(':average', $average, PDO::PARAM_STR);
+                $updateQuery->bindParam(':attraction_id', $attraction_id, PDO::PARAM_INT);
+                $updateQuery->execute();
+
                 echo json_encode(['success' => true, 'message' => 'Comment added successfully']);
             } else {
                 echo json_encode(['success' => false, 'message' => 'Failed to add comment']);
@@ -53,5 +50,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 } else {
     echo json_encode(['success' => false, 'message' => 'Invalid request method']);
+}
+function calculateAttractionAverageRating($attractionId, $pdo) {
+    $sqlEvaluation = "SELECT SUM(evaluation) FROM comments WHERE attraction_id = :attraction_id";
+    $queryEvaluation = $pdo->prepare($sqlEvaluation);
+    $queryEvaluation->bindParam(':attraction_id', $attractionId, PDO::PARAM_INT);
+    $queryEvaluation->execute();
+    $evaluations = $queryEvaluation->fetchAll(PDO::FETCH_COLUMN);
+
+    $sqlCount = "SELECT COUNT(*) AS count FROM comments WHERE attraction_id = :attraction_id";
+    $queryCount = $pdo->prepare($sqlCount);
+    $queryCount->bindParam(':attraction_id', $attractionId, PDO::PARAM_INT);
+    $queryCount->execute();
+    $resultCount = $queryCount->fetch(PDO::FETCH_ASSOC);
+
+    $count = $resultCount['count'];
+    $total = array_sum($evaluations);
+
+    if ($count > 0) {
+        $average = $total / $count;
+    } else {
+        $average = 0;
+    }
+
+    return $average;
 }
 ?>
